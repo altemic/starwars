6b008316c1a1491c24542503e3f3ba34b3563463
Michał Altenberg <altemic@gmail.com>
2020-12-31 10:32:11 +0100

    Implemented EF Core part.

---
+++ RatingCenter/Entities/FilmContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entities
+{
+    public class FilmsContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<FilmRating>(entity =>
+            {
+                entity.HasIndex(e => e.ExternalId);
+            });
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseInMemoryDatabase("Films");
+        }
+
+        public virtual DbSet<FilmRating> FilmRatings { get; set; }
+
+        public new IQueryable<T> Set<T>() where T : class => base.Set<T>();
+    }
+}
---
+++ RatingCenter/Entities/Models/FilmRating.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Entities.Models
+{
+    [Table("film_ratings")]
+    public class FilmRating
+    {
+        [Column("filmrating_id")]
+        public int FilmRatingId { get; set; }
+
+        [Column("external_id")]
+        public string ExternalId { get; set; }
+
+        [Column("rating")]
+        public int Rating { get; set; }
+
+        [Column("nickname")]
+        public string Nickname { get; set; }
+
+        [Column("description")]
+        public string Description { get; set; }
+    }
+}
