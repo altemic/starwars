c139d47a7a14eaae28447256e7159b58483c1f40
Michał Altenberg <altemic@gmail.com>
2021-01-05 07:19:16 +0100

    Removed unused usings

--- RatingCenter/Business/DI.cs
+++ RatingCenter/Business/DI.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Business.Services;
+﻿using Business.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Business
--- RatingCenter/Business/Services/FilmsService.cs
+++ RatingCenter/Business/Services/FilmsService.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
-using Business.Dtos;
 using Entities;
-using Entities.Models;
 using Integrations.StarWars;
-using Microsoft.EntityFrameworkCore;
 
 namespace Business.Services
 {
